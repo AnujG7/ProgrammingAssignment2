9ee9a110de1f3814c6af0c2347dc95a2d3f888ea
Sahil Singh <sahil.singh@autovrse.in>
2020-07-27 00:14:32 +0530

    First Program

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,16 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+MyInvMat <<- NULL
+set <- function(y){
+  x <<- y
+  MyInvMat <<- NULL
+}
+get <- function() x
+setInverse <- function(inverse) MyInvMat <<- inverse
+getInverse <- function() y
+
+list(set = set, get = get, setInverse = setInverse, get = getInverse)
 
 }
 
